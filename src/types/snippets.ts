c5a2d5e0dd7d253decab0065ce978e0da5972ba1
Nurmx7292 <raxmetnur04@gmail.com>
2025-10-06 13:27:29 +0200

    feat: add types for snippets and user beforehand to create user and snippet service

---
+++ src/types/snippets.ts
@@ -0,0 +1,43 @@
+export interface User {
+  id: number
+  username: string
+  role: string
+}
+
+export interface Snippet {
+  id: number
+  language: string
+  code: string
+  user: User
+}
+
+export interface SnippetsResponse {
+  data: Snippet[]
+  meta: {
+    itemsPerPage: number
+    totalItems: number
+    currentPage: number
+    totalPages: number
+    sortBy: [string, string][]
+    searchBy: string[]
+    search: string
+    select: string[]
+    filter: Record<string, any>
+  }
+  links: {
+    first: string
+    previous: string
+    current: string
+    next: string
+    last: string
+  }
+}
+
+export interface SnippetsParams {
+  userId?: number
+  page?: number
+  limit?: number
+  sortBy?: string[]
+  search?: string
+  searchBy?: string[]
+}
