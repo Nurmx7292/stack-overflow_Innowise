3e1b8ea08d6e34d7d03faac323b2e07d4378cb07
Nurmx7292 <raxmetnur04@gmail.com>
2025-10-06 03:00:37 +0200

    feat: setup the routes for the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import Home from './pages/Home/Home'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+import Questions from './pages/Questions/Questions'
+import Users from './pages/Users/Users'
+import Account from './pages/Account/Account'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="questions" element={<Questions />} />
+        <Route path="users" element={<Users />} />
+        <Route path="account" element={<Account />} />
+        <Route path="posts/create" element={<div>Create Post</div>} />
+        <Route path="posts/my" element={<div>My Posts</div>} />
+      </Route>
+    </Routes>
   )
 }
 
