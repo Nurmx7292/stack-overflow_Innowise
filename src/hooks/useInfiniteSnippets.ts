f39ebe5d8be5470d365e5aee7d9fd7162447549c
Nurmx7292 <raxmetnur04@gmail.com>
2025-10-08 00:09:51 +0200

    feat: add useInfiniteSnippets hook to implement infinite scroll

---
+++ src/hooks/useInfiniteSnippets.ts
@@ -0,0 +1,17 @@
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { snippetsService } from '../services/snippetsService'
+import type { SnippetsParams } from '../types/snippets'
+
+export const useInfiniteSnippets = (params: Omit<SnippetsParams, 'page'> = {}) => {
+  return useInfiniteQuery({
+    queryKey: ['snippets', 'infinite', params],
+    queryFn: ({ pageParam = 1 }) => 
+      snippetsService.getSnippets({ ...params, page: pageParam as number }),
+    getNextPageParam: (lastPage) => {
+      return lastPage.meta.currentPage < lastPage.meta.totalPages 
+        ? lastPage.meta.currentPage + 1 
+        : undefined
+    },
+    initialPageParam: 1,
+  })
+}
