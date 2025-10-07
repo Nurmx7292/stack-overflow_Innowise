5cfcee92e754cf90b2f1fc3053391fa35dec535e
Nurmx7292 <raxmetnur04@gmail.com>
2025-10-07 11:42:04 +0200

    feat: use snippets hook, connet service to Home page, add styling for the snippets

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { queryClient } from './lib/queryClient'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "./lib/queryClient";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,20 @@
+import { useSnippets } from '../../hooks/useSnippets'
+import SnippetCard from '../../components/SnippetCard/SnippetCard'
+import styles from './Home.module.css'
+
 export default function Home() {
+  const { data, isLoading, error } = useSnippets({ page: 1, limit: 15 })
+  if (isLoading) return <div className={styles.loading}>Loading...</div>
+  if (error) return <div className={styles.error}>Error loading snippets</div>
+
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>This is the home page</p>
+    <div className={styles.page}>
+      <h1 className={styles.title}>Welcome to Codelang! &lt;/&gt;</h1>
+      <div className={styles.snippetsGrid}>
+        {data?.data.map(snippet => (
+          <SnippetCard key={snippet.id} snippet={snippet} />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
