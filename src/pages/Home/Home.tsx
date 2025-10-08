085de5c6320d4a5a68acb9b6801c0a51e824ceda
Nurmx7292 <raxmetnur04@gmail.com>
2025-10-08 11:46:10 +0200

    refactor: Replace Home page layout to SnippetsList component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,10 @@
-import { useSnippets } from '../../hooks/useSnippets'
-import SnippetCard from '../../components/SnippetCard/SnippetCard'
+import SnippetsList from '../../components/SnippetsList/SnippetsList'
 import styles from './Home.module.css'
 
 export default function Home() {
-  const { data, isLoading, error } = useSnippets({ page: 1, limit: 15 })
-  if (isLoading) return <div className={styles.loading}>Loading...</div>
-  if (error) return <div className={styles.error}>Error loading snippets</div>
-
   return (
     <div className={styles.page}>
-      <h1 className={styles.title}>Welcome to Codelang! &lt;/&gt;</h1>
-      <div className={styles.snippetsGrid}>
-        {data?.data.map(snippet => (
-          <SnippetCard key={snippet.id} snippet={snippet} />
-        ))}
-      </div>
+      <SnippetsList />
     </div>
   )
 }
