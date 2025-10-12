4dc8f38e9f45c6c12b357fc41f00cb0d3c4aaafb
Nurmx7292 <raxmetnur04@gmail.com>
2025-10-13 01:06:29 +0200

    feat: connect the userApiService to create React Query hooks, utilize all implemented service functions

---
+++ src/hooks/useUserApi.ts
@@ -0,0 +1,81 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { userApiService } from '../services/userApiService'
+import { userService } from '../services/userService'
+import type { 
+  UpdateUserData, 
+  UpdatePasswordData, 
+  UsersParams 
+} from '../types/user'
+
+export const useCurrentUser = () => {
+  return useQuery({
+    queryKey: ['currentUser'],
+    queryFn: userApiService.getCurrentUser,
+    enabled: userService.isAuthenticated(),
+    staleTime: 5 * 60 * 1000,
+    retry: 3
+  })
+}
+
+export const useUserById = (id: number) => {
+  return useQuery({
+    queryKey: ['user', id],
+    queryFn: () => userApiService.getUserById(id),
+    enabled: !!id,
+    staleTime: 5 * 60 * 1000,
+    retry: 3
+  })
+}
+
+export const useUserStatistic = (id: number) => {
+  return useQuery({
+    queryKey: ['userStatistic', id],
+    queryFn: () => userApiService.getUserStatistic(id),
+    enabled: !!id,
+    staleTime: 5 * 60 * 1000,
+    retry: 3
+  })
+}
+
+export const useUsers = (params: UsersParams = {}) => {
+  return useQuery({
+    queryKey: ['users', params],
+    queryFn: () => userApiService.getUsers(params),
+    staleTime: 5 * 60 * 1000,
+    retry: 3
+  })
+}
+
+export const useUpdateUser = () => {
+  const queryClient = useQueryClient()
+  
+  return useMutation({
+    mutationFn: userApiService.updateUser,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ['currentUser'] })
+      queryClient.invalidateQueries({ queryKey: ['user'] })
+      queryClient.invalidateQueries({ queryKey: ['users'] })
+    },
+    retry: 1
+  })
+}
+
+export const useUpdatePassword = () => {
+  return useMutation({
+    mutationFn: userApiService.updatePassword,
+    retry: 1
+  })
+}
+
+export const useDeleteUser = () => {
+  const queryClient = useQueryClient()
+  
+  return useMutation({
+    mutationFn: userApiService.deleteUser,
+    onSuccess: () => {
+      queryClient.clear()
+      userService.removeUser()
+    },
+    retry: 1
+  })
+}
