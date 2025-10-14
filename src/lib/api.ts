796d7ab72b97539f5c0e273e186738c1b14e5998
Nurmx7292 <raxmetnur04@gmail.com>
2025-10-14 15:42:14 +0200

    feat: setup proxy server via Vite to avoid CORS policy issues

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://codelang.vercel.app'
-
 export const api = axios.create({
-  baseURL: API_BASE_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
