1897fb75a55578099c1f2986de3e726e3086f345
Nurmx7292 <raxmetnur04@gmail.com>
2025-10-16 12:05:56 +0200

    feat: add Q&A services, add types for them

---
+++ src/types/questions.ts
@@ -0,0 +1,76 @@
+export interface User {
+  id: string
+  username: string
+  role: string
+}
+
+export interface Answer {
+  id: string
+  content: string
+  questionId: string
+  user: User
+  isCorrect: boolean
+}
+
+export interface Question {
+  id: string
+  title: string
+  description: string
+  attachedCode?: string
+  user: User
+  answers?: Answer[]
+  isResolved: boolean
+}
+
+export interface QuestionsParams {
+  page?: number
+  limit?: number
+  sortBy?: string[]
+  search?: string
+  searchBy?: string[]
+}
+
+export interface QuestionsResponse {
+  data: Question[]
+  meta: {
+    itemsPerPage: number
+    totalItems: number
+    currentPage: number
+    totalPages: number
+    sortBy: [string, 'ASC' | 'DESC'][]
+    searchBy: string[]
+    search: string
+    select: string[]
+    filter: Record<string, any>
+  }
+  links: {
+    first: string
+    previous: string
+    current: string
+    next: string
+    last: string
+  }
+}
+
+export interface CreateQuestionRequest {
+  title: string
+  description: string
+  attachedCode?: string
+}
+
+export interface UpdateQuestionRequest {
+  title?: string
+  description?: string
+  attachedCode?: string
+}
+
+export interface CreateAnswerRequest {
+  content: string
+  questionId: string
+}
+
+export interface UpdateAnswerRequest {
+  content: string
+}
+
+export type AnswerState = 'correct' | 'incorrect'
